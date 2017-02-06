c98187a60c3d69aa12202cb8ae34f01b89ba3c73
pablo stellet <pablorstellet@gmail.com>
2017-02-06 02:42:02 -0300

    This is our first step into xamarin using each plataform. We are good to go in these courses!

---
+++ HelloWorld/HelloXamarin/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloXamarin
+{
+    [Activity(Label = "HelloXamarin", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            //Get our button from the layout resource.
+            //And Attach event to it
+            Button button = FindViewById<Button>(Resource.Id.button1);
+
+            button.Click += delegate
+            {
+                //button.Text = string.Format("STOP THIS PERV! YOU TOUCHED {0} TIMES ALREADY", count++);
+                string text = string.Empty;
+                switch (count)
+                {
+                    case 1:
+                        text = "You touch my tralala ~";
+                        break;
+                    case 2:
+                        text = "My ding ding dong";
+                        break;
+                    case 3:
+                        text = "Ok.. you can stop now..";
+                        break;
+                    default:
+                        text = string.Format("STOP THIS PERV! YOU TOUCHED {0} TIMES ALREADY", count);
+                        break;
+                }
+                button.Text = text;
+                count++;
+            };
+
+        }
+    }
+}
+
---
+++ HelloWorld/HelloXamarinIos/ViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using UIKit;
+
+namespace HelloXamarinIos
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {            
+         
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib. Manually create the label:
+            //UILabel helloLabel;
+            //helloLabel = new UILabel {
+            //    Frame = new CoreGraphics.CGRect(50, 50, 120, 40),
+            //    Text = "Hello World"
+            //};
+
+            //View.AddSubview(helloLabel);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
