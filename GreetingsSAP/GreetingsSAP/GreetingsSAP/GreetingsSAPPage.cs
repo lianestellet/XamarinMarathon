5606ba95830e10124474c47b7ebb3902dfacda8d
pablo stellet <pablorstellet@gmail.com>
2017-02-22 21:07:41 -0300

    Shared Application solution
    Android, Windows Phone and Iphone

---
+++ GreetingsSAP/GreetingsSAP/GreetingsSAP/GreetingsSAPPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GreetingsSAP
+{
+	public class GreetingsSAPPage : ContentPage
+	{
+		public GreetingsSAPPage ()
+		{
+            var MyLabel = new Label();
+            MyLabel.Text = "Greetings, Xamarin.Forms!";
+            this.Content = MyLabel;
+
+            MyLabel.HorizontalOptions = LayoutOptions.Center;
+            MyLabel.VerticalOptions = LayoutOptions.Center;
+            MyLabel.HorizontalTextAlignment = TextAlignment.Center;
+            MyLabel.VerticalTextAlignment = TextAlignment.Center;
+
+            Padding = Device.OnPlatform<Thickness>(
+             new Thickness(0, 20, 0, 0),
+             new Thickness(0),
+             new Thickness(0));
+
+        }
+    }
+}
---
+++ XamarinSharedForms/XamarinPCL/XamarinPCL/GreetingsPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinPCL
+{
+    public class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            var MyLabel = new Label();
+            MyLabel.Text = "Greetings, Xamarin.Forms!";
+            this.Content = MyLabel;
+            Padding = new Thickness(0, 20, 0, 0);
+
+            //Content = new StackLayout
+            //{
+            //    Children = {
+            //        new Label { Text = "Hello Page" }
+            //    }
+            //};
+        }
+    }
+}
